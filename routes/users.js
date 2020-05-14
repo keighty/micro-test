a132da94d86097a7fa5a5a368b204da9a671ad75
keighty <k80.leonard@gmail.com>
2020-05-13 23:06:01 -0700

    feat(routes): add user collection to routes

--- routes/users.js
+++ routes/users.js
@@ -1,9 +1,38 @@
 const express = require('express');
 const router = express.Router();
+const UserCollection = require('../data/user_collection')
+
+const userCollection = new UserCollection()
+
 
 /* curl http://localhost:3000/users */
 router.get('/', function(req, res, next) {
-  res.send("hello users")
+  res.send(userCollection.getAll())
+});
+
+/* GET user. */
+router.get('/:userId', function (req, res, next) {
+  const userId = req.params.userId
+  const user = userCollection.getUser(userId)
+  res.send(user);
+});
+
+/* curl -X POST http://localhost:3000/users/create */
+router.post('/create', function (req, res, next) {
+  const data = req.body
+  const user = userCollection.addUser(data)
+  res.send("ha")
+});
+
+
+/* UPDATE user. */
+router.get('/update', function (req, res, next) {
+  res.send('respond with a resource');
+});
+
+/* DELETE user. */
+router.get('/delete', function (req, res, next) {
+  res.send('respond with a resource');
 });
 
 module.exports = router;
