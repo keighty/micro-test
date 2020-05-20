b77c5d3d0baba4f44eae0994f8a0cc24458fc054
keighty <k80.leonard@gmail.com>
2020-05-19 20:54:36 -0700

    chore: remove unused boilerplate

--- app.js
+++ app.js
@@ -1,6 +1,5 @@
 const express = require('express')
 const path = require('path')
-const favicon = require('serve-favicon')
 const logger = require('morgan')
 const cookieParser = require('cookie-parser')
 const bodyParser = require('body-parser')
@@ -10,12 +9,6 @@ const users = require('./routes/users')
 
 const app = express()
 
-// view engine setup
-app.set('views', path.join(__dirname, 'views'))
-app.set('view engine', 'jade')
-
-// uncomment after placing your favicon in /public
-//app.use(favicon(path.join(__dirname, 'public', 'favicon.ico')));
 app.use(logger('dev'))
 app.use(bodyParser.json())
 app.use(bodyParser.urlencoded({ extended: false }))
